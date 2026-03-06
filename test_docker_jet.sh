94b93b9ee36592f24c15bbe199a643f816a10ed2
huluxiaohuowa <jianxin.hu@ictrek.com>
2026-03-06 15:17:55 +0800

    chore: 更新 Docker 测试脚本配置

--- test_docker_jet.sh
+++ test_docker_jet.sh
@@ -12,11 +12,12 @@ docker run -dit \
   -e TRANSFORMERS_OFFLINE=1 \
   -e HF_DATASETS_OFFLINE=1 \
   -e MAX_WORKERS=3 \
-  -e PER_MODEL_MAX=2 \
+  -e PER_MODEL_MAX=1 \
   -e IDLE_TIMEOUT_S=900 \
   -e MODEL_PORT_BASE=18008 \
   -e PYTHON_BIN=python3 \
+  -e PRELOAD_MODELS=vit-b-16-siglip2__webli \
+  -e PIN_MODELS=vit-b-16-siglip2__webli \
   -v /home/jhu/dev/models/embs:/root/models \
   -v /home/jhu/dev/media:/root/media \
-  -v /etc/localtime:/etc/localtime:ro \
   emb_server
